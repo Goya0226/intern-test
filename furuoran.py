c52324962ecf14653fa2c1e7fc715c90118ba8aa
furuoran <35084760+Goya0226@users.noreply.github.com>
2018-02-28 16:34:14 +0800

    Add files via upload

---
+++ furuoran.py
@@ -0,0 +1,14 @@
+
+from __future__ import division 
+
+
+
+def divide( a, b ):
+   
+   
+   return a/b;
+    a = input("Please input a:") 
+b= input("Please input b:") 
+   result = divide(int(a),int(b))
+ 
+    print(result)
